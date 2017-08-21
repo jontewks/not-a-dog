31658a31bcd16c5378a483f680981b2518e34249
Jon Tewksbury <jontewks@gmail.com>
2017-08-20 21:09:18 -0700

    Add base of VGG network for cats vs dogs image recognition

---
+++ utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] != 3):
+            ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    cols = len(ims)//rows if len(ims) % 2 == 0 else len(ims)//rows + 1
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, cols, i+1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i], interpolation=None if interp else 'none')
